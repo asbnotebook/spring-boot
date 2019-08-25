db74c607d9521052eab397291830e9169720665a
asbnotebook <arunsb1339@gmail.com>
2019-08-25 15:08:15 +0530

    Create Employee.java

---
+++ spring-boot-h2-config-example/src/main/java/com/asb/example/Employee.java
@@ -0,0 +1,25 @@
+package com.asb.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
