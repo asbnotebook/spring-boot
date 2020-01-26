44bbc181bc43d17fffdc1df851a9184ed7483555
asbnotebook <asbnotebook@gmail.com>
2020-01-26 10:18:18 +0530

    Added missing annotation

--- spring-boot-hazelcast-example/src/main/java/com/asb/example/config/HazelCastConfig.java
+++ spring-boot-hazelcast-example/src/main/java/com/asb/example/config/HazelCastConfig.java
@@ -1,6 +1,7 @@
 package com.asb.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
@@ -11,6 +12,7 @@ import com.hazelcast.config.MaxSizeConfig.MaxSizePolicy;
 @Configuration
 public class HazelCastConfig {
 
+	@Bean
 	public Config config() {
 		return new Config()
 				.setInstanceName("hazelcast-instance")
