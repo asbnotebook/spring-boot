82205ec1d56fd27fd013f40745b57c08fd4268f2
asbnotebook <asbnotebook@gmail.com>
2020-06-17 10:13:01 +0530

    Update Course.java

--- jpa-many-to-many-example/src/main/java/com/asb/example/model/Course.java
+++ jpa-many-to-many-example/src/main/java/com/asb/example/model/Course.java
@@ -4,12 +4,9 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -33,9 +30,7 @@ public class Course {
 	@Column(name = "name")
 	private String name;
 
-	@ManyToMany(fetch = FetchType.LAZY)
+	@ManyToMany(mappedBy = "courses")
 	@JsonIgnore
-	@JoinTable(name = "STUDENT_COURSE", joinColumns = { @JoinColumn(name = "COURSE_ID") }, inverseJoinColumns = {
-			@JoinColumn(name = "STUDENT_ID") })
 	private Set<Student> students;
 }
