13e181d803189b181e8ea408b49f906d6efb9eab
asbnotebook <asbnotebook@gmail.com>
2020-06-17 10:18:06 +0530

    Update StudentController.java

--- jpa-many-to-many-example/src/main/java/com/asb/example/controller/StudentController.java
+++ jpa-many-to-many-example/src/main/java/com/asb/example/controller/StudentController.java
@@ -7,13 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.asb.example.model.Student;
+import com.asb.example.dto.StudentDto;
 import com.asb.example.service.StudentService;
 
 @RestController
@@ -23,29 +23,26 @@ public class StudentController {
 	private StudentService studentService;
 
 	@GetMapping("/students")
-	public ResponseEntity<List<Student>> getAllStudents() {
-
-		List<Student> students = studentService.getAllStudents();
+	public ResponseEntity<List<StudentDto>> getAllStudents() {
+		List<StudentDto> students = studentService.getAllStudents();
 		return new ResponseEntity<>(students, HttpStatus.OK);
 	}
 
 	@PostMapping("/student")
-	public ResponseEntity<Student> getAllStudents(@RequestBody Student student) {
-
-		Student std = studentService.addStudent(student);
+	public ResponseEntity<StudentDto> getAllStudents(@RequestBody StudentDto studentDto) {
+		StudentDto std = studentService.addStudent(studentDto);
 		return new ResponseEntity<>(std, HttpStatus.CREATED);
 	}
 
-	@PutMapping("/student")
-	public ResponseEntity<Student> updateEmployee(@RequestBody Student student) {
-
-		Student std = studentService.addStudent(student);
+	@PutMapping("/student/{id}")
+	public ResponseEntity<StudentDto> updateEmployee(@PathVariable(name = "id") Integer id,
+			@RequestBody StudentDto student) {
+		StudentDto std = studentService.updateStudent(id, student);
 		return new ResponseEntity<>(std, HttpStatus.CREATED);
 	}
 
-	@DeleteMapping("/student")
-	public ResponseEntity<String> deleteStudent(@RequestParam Integer studentId) {
-
+	@DeleteMapping("/student/{id}")
+	public ResponseEntity<String> deleteStudent(@PathVariable(name = "id") Integer studentId) {
 		String message = studentService.deleteStudent(studentId);
 		return new ResponseEntity<>(message, HttpStatus.OK);
 	}
