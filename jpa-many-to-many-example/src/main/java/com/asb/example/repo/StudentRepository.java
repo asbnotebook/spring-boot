5701d497cfeab34c7137f02339f8ff38e68cb2a9
asbnotebook <asbnotebook@gmail.com>
2020-06-17 10:19:36 +0530

    Update StudentRepository.java

--- jpa-many-to-many-example/src/main/java/com/asb/example/repo/StudentRepository.java
+++ jpa-many-to-many-example/src/main/java/com/asb/example/repo/StudentRepository.java
@@ -1,11 +1,9 @@
 package com.asb.example.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.asb.example.model.Student;
 
-@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
