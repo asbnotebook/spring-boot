d3578b1f0b375c65419470235b9eac9930748ac2
asbnotebook <asbnotebook@gmail.com>
2020-06-17 10:12:14 +0530

    Update Student.java

--- jpa-many-to-many-example/src/main/java/com/asb/example/model/Student.java
+++ jpa-many-to-many-example/src/main/java/com/asb/example/model/Student.java
@@ -1,5 +1,6 @@
 package com.asb.example.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -31,8 +32,24 @@ public class Student {
 	@Column(name = "name")
 	private String name;
 
-	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "STUDENT_COURSE", joinColumns = { @JoinColumn(name = "COURSE_ID") }, inverseJoinColumns = {
-			@JoinColumn(name = "STUDENT_ID") })
+	@ManyToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+	@JoinTable(name = "STUDENT_COURSE", joinColumns = { @JoinColumn(name = "STUDENT_ID") }, inverseJoinColumns = {
+			@JoinColumn(name = "COURSE_ID") })
 	private Set<Course> courses;
+
+	public void addCourse(Course course) {
+		this.courses.add(course);
+		course.getStudents().add(this);
+	}
+
+	public void removeCourse(Course course) {
+		this.getCourses().remove(course);
+		course.getStudents().remove(this);
+	}
+
+	public void removeCourses() {
+		for (Course course : new HashSet<>(courses)) {
+			removeCourse(course);
+		}
+	}
 }
