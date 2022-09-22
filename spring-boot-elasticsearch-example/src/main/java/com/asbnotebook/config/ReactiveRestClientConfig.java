3f97e3c15bd3eea9e032a2908d8c3513ca369a4f
asbnotebook <asbnotebook@gmail.com>
2022-09-22 22:31:14 +0530

    removed extra bean annotation

--- spring-boot-elasticsearch-example/src/main/java/com/asbnotebook/config/ReactiveRestClientConfig.java
+++ spring-boot-elasticsearch-example/src/main/java/com/asbnotebook/config/ReactiveRestClientConfig.java
@@ -1,6 +1,5 @@
 package com.asbnotebook.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient;
@@ -10,7 +9,6 @@ import org.springframework.data.elasticsearch.config.AbstractReactiveElasticsear
 @Configuration
 public class ReactiveRestClientConfig extends AbstractReactiveElasticsearchConfiguration {
     @Override
-    @Bean
     public ReactiveElasticsearchClient reactiveElasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("localhost:9200")
