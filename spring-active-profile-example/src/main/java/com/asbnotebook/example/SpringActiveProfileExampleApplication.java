cb6d0b726eae6888d3493e49a6fdfc403755877f
arun <arun.s.b@ibm.com>
2025-02-04 19:17:09 +0530

    upgrading spring boot version

---
+++ spring-active-profile-example/src/main/java/com/asbnotebook/example/SpringActiveProfileExampleApplication.java
@@ -0,0 +1,13 @@
+package com.asbnotebook.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActiveProfileExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActiveProfileExampleApplication.class, args);
+	}
+
+}
---
+++ spring-active-profile-example/src/test/java/com/asbnotebook/example/SpringActiveProfileExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asbnotebook.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActiveProfileExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
